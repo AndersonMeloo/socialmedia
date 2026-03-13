8657c6ce1103289f22bf65f2b05cc7bf8dd22f98
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-12 23:38:02 -0300

    feat: Adicionando Validações class-validator, CreateUserDto

--- backend/src/modules/users/dto/create-user.dto.ts
+++ backend/src/modules/users/dto/create-user.dto.ts
@@ -1,14 +1,34 @@
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail()
+  @IsNotEmpty({ message: `Preencha o campo` })
+  @IsEmail({ message: `Endereço de e-mail inválido` })
   email: string;
 
   @IsOptional()
-  @IsString()
+  @IsString({ message: `Preencha o campo` })
+  @MinLength(3, {
+    message: `O Nome deve conter no mínimo 3 caracteres`,
+  })
+  @MaxLength(10, {
+    message: `O Nome deve conter no máximo 10 caracteres`,
+  })
   name?: string;
 
   @IsOptional()
-  @IsString()
+  @IsString({ message: `Preencha o campo` })
+  @MinLength(3, {
+    message: `A Senha deve conter no mínimo 3 caracteres`,
+  })
+  @MaxLength(20, {
+    message: `A Senha deve conter no máximo 20 caracteres`,
+  })
   password?: string;
 }
