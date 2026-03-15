889871fd7eeaa31d13e5d466fff037e7cd46044f
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-15 12:52:10 -0300

    feat(auth) Importando todo sistewma de AuthModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
     UsersModule,
   ],
   controllers: [AppController],
