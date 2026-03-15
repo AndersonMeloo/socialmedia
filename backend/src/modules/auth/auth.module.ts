1f0e646f1143e10780e44a91adf36a5e42f2bef1
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-03-15 12:46:53 -0300

    feat(auth) Cria AuthModule - Importa UsersModule e JwtModule - Configura JWT com chave secreta e expira em 1h - Registra AuthController e AuthService

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: 'your_jwt_secret_key', // Substitua por uma chave secreta forte
+      signOptions: { expiresIn: '1h' }, // Token expira em 1 hora
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
