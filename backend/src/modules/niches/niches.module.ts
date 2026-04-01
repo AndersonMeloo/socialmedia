d08bcf194fe980dfe3ad2e38751405ea917824bb
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-04-01 20:47:06 -0300

    feat(niches-module) Conectando Prisma ao Modulo de Nichos - PrismaModule

--- backend/src/modules/niches/niches.module.ts
+++ backend/src/modules/niches/niches.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NichesService } from './niches.service';
 import { NichesController } from './niches.controller';
+import { PrismaModule } from 'src/database/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [NichesController],
   providers: [NichesService],
 })
