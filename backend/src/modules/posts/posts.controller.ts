64dffa6589b685f940146d36e6fc395ef24460c1
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-04-01 21:03:04 -0300

    feat: - package.json e package-lock.json - @types/passport-google-oauth20 - Npm Contexto: anotação interna de comandos de instalação - Routes Novas Features do que foi feito e testes

--- backend/src/modules/posts/posts.controller.ts
+++ backend/src/modules/posts/posts.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ImportYoutubePostDto } from './dto/import-youtube-post.dto';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Post('import-youtube-url')
+  importYoutubeUrl(@Body() body: ImportYoutubePostDto) {
+    return this.postsService.createPostFromYoutubeUrl(body);
+  }
+}
