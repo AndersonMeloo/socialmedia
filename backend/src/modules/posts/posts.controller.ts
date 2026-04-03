1d19768ee54f8f28c6714e0ce12356c122cb4d13
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-04-02 22:45:36 -0300

    feat(posts.controller): ENDPOINTS GET para Listagem  Overview de Posts

--- backend/src/modules/posts/posts.controller.ts
+++ backend/src/modules/posts/posts.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ImportYoutubePostDto } from './dto/import-youtube-post.dto';
 import { PostsService } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  findAllPosts() {
+    return this.postsService.listPosts();
+  }
+
+  @Get('overview')
+  getOverview(@Query('date') date?: string) {
+    return this.postsService.getPostsOverviewByDate(date);
+  }
 
   @Post('import-youtube-url')
   importYoutubeUrl(@Body() body: ImportYoutubePostDto) {
